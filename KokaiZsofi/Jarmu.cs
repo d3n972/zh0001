98381d26a25a255894aae6f0cee52f024136609d
d3n <d3n@DESKTOP-7EO54G0>
2022-11-26 15:15:30 +0000

    Add project files.

---
+++ KokaiZsofi/Jarmu.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KokaiZsofi
+{
+    enum Kategoria
+    {
+        Dolgozo,
+        Vendeg,
+        Mozgasserult
+    }
+
+
+    class Jarmu
+    {
+        private enum recordFields
+        {
+            entryIndex,
+            plate,
+            arrival,
+            departure,
+            category
+        }
+
+        private int ParkolasiIdo
+        {
+            get => parkolasiIdo; set
+            {
+                parkolasiIdo = value - this.erkezesiIdo;
+                if (parkolasiIdo < 0)
+                {
+                    parkolasiIdo = 0;
+                }
+            }
+        }
+        private int erkezesiIdo { get; set; }
+        private string rendszam;
+        private int parkolasiIdo;
+
+        private Kategoria kategoria { get; set; }
+        public string Rendszam
+        {
+            get => rendszam; set
+            {
+                if (Regex.Match(value, "/\\w{3}-\\d{3}/").Success)
+                {
+                    rendszam = value;
+                }
+            }
+        }
+
+        public Jarmu(string mapEntry)
+        {
+            string[] fields = mapEntry.Split(' ');
+            for (int idx = 0; idx < fields.Length; idx++)
+            {
+                switch ((recordFields)idx)
+                {
+                    case recordFields.entryIndex:
+                        break;
+                    case recordFields.plate:
+                        this.Rendszam = fields[idx];
+                        break;
+                    case recordFields.departure:
+                        this.ParkolasiIdo = Convert.ToInt32(fields[idx]);
+                        break;
+                    case recordFields.arrival:
+                        this.erkezesiIdo = Convert.ToInt32(fields[idx]);
+                        break;
+                    case recordFields.category:
+                        switch (fields[idx])
+                        {
+                            case "D": this.kategoria = Kategoria.Dolgozo; break;
+                            case "M": this.kategoria = Kategoria.Mozgasserult; break;
+                            case "V": this.kategoria = Kategoria.Vendeg; break;
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+        public int ParkolasiDij()
+        {
+            if (
+                this.kategoria != Kategoria.Vendeg
+                || this.parkolasiIdo < 900
+                )
+            {
+                return 0;
+            }
+            int hoursToPay = this.parkolasiIdo / (60 * 60) + (this.parkolasiIdo % (60 * 60) > 0 ? 1 : 0);
+            return hoursToPay * 500;
+        }
+        public override string ToString()
+        {
+            return String.Format(
+                @"{0} Érk: {1}, Tot: {2}, Fiz: {3} Ft",
+                this.Rendszam,
+                this.erkezesiIdo,
+                this.parkolasiIdo,
+                this.ParkolasiDij()
+                );
+        }
+    }
+}
---
+++ KokaiZsofi/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KokaiZsofi;
+
+string[] lines = new string[] {
+"s1 ABV-043 1220 2102 D",
+"s2 GLS-031 2220 6705 V",
+"s3 AHR-140 3230 3992 V",
+"s4 MBS-204 4160 5806 V",
+"s5 ABV-043 5220 6102 D",
+"s6 MSV-146 5610 6723 M",
+};
+List<Jarmu> j = new List<Jarmu>();
+foreach (var item in lines)
+{
+    j.Add(new Jarmu(item));
+}
+
+j.ForEach(x => Console.WriteLine(x));
